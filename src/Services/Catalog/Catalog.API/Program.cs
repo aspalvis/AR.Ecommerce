5a78808912468e713d46bf11c458dcdd93cad315
Andris Spalvis <spalvis.andris@gmail.com>
2024-03-20 09:33:20 +0200

    Catalog: Folder structure refactoring. Added common layer. CQRS implementation

---
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -0,0 +1,19 @@
+using Catalog.API.Products.CreateProduct;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCarter(configurator: c =>
+{
+    c.WithModule<CreateProductEndpoint>();
+});
+
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+});
+
+var app = builder.Build();
+
+app.MapCarter();
+
+app.Run();
