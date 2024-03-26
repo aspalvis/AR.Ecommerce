b804187e3cab5a73f454f1359eb17f076ace5784
Andris Spalvis <spalvis.andris@gmail.com>
2024-03-26 19:28:51 +0200

    BuildingBlocks: LoggingBehavior, with injecting to Catalog.API

---
+++ src/BuildingBlocks/BuildingBlocks/Behaviors/LoggingBehavior.cs
@@ -0,0 +1,36 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+
+namespace BuildingBlocks.Behaviors
+{
+    public class LoggingBehavior<TRequest, TResponse>(ILogger<LoggingBehavior<TRequest, TResponse>> logger)
+        : IPipelineBehavior<TRequest, TResponse>
+        where TRequest : notnull, IRequest<TResponse>
+        where TResponse : notnull
+    {
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("[START] Hadle request={Request} - Response={Response} - RequestData={RequestData}",
+                typeof(TRequest).Name, typeof(TResponse).Name, request);
+
+            var timer = new Stopwatch();
+            timer.Start();
+
+            TResponse response = await next();
+
+            timer.Stop();
+
+            TimeSpan timeTaken = timer.Elapsed;
+
+            if (timeTaken.Seconds > 3)
+            {
+                logger.LogWarning("[PERFORMANCE] The request {Request} took {TimeTaken} seconds", typeof(TRequest).Name, timeTaken.Seconds);
+            }
+
+            logger.LogInformation("[END] Hadled {Request} with {Response}", typeof(TRequest).Name, typeof(TResponse).Name);
+
+            return response;
+        }
+    }
+}
