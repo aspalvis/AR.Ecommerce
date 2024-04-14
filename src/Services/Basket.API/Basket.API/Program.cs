0adbae59dda2ae157720454b585e9409f5434190
Andris Spalvis <spalvis.andris@gmail.com>
2024-04-14 13:07:04 +0300

    Basket - Exception handler

--- src/Services/Basket.API/Basket.API/Program.cs
+++ src/Services/Basket.API/Basket.API/Program.cs
@@ -1,4 +1,5 @@
 using Basket.API.Data;
+using BuildingBlocks.Exceptions.Handler;
 using Marten;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -21,8 +22,11 @@ services.AddMarten(c =>
 
 services.AddScoped<IBasketRepository, BasketRepository>();
 
+services.AddExceptionHandler<CustomExceptionHandler>();
+
 var app = builder.Build();
 
 app.MapCarter();
+app.UseExceptionHandler(options => { });
 
 app.Run();
