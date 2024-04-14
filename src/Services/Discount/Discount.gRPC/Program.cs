a7bc9ae7adcc4ea41cb8446c2621b412e49aa0f8
Andris Spalvis <spalvis.andris@gmail.com>
2024-04-15 00:14:59 +0300

    Discount - Initialized Discount.gRPC project, with docker support

---
+++ src/Services/Discount/Discount.gRPC/Program.cs
@@ -0,0 +1,11 @@
+using Discount.gRPC.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddGrpc();
+
+var app = builder.Build();
+
+app.MapGrpcService<DiscountService>();
+app.Run();
