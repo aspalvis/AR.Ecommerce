9f74afe210c37a820e5abd05399cf5b6e247f0c8
Andris Spalvis <spalvis.andris@gmail.com>
2024-04-20 18:33:23 +0300

    Ordering.Domain - Domain exception, and private constructors for value objects

--- src/Services/Ordering/Ordering.Domain/ValueObjects/OrderId.cs
+++ src/Services/Ordering/Ordering.Domain/ValueObjects/OrderId.cs
@@ -1,7 +1,23 @@
-﻿namespace Ordering.Domain.ValueObjects
+﻿using Ordering.Domain.Exceptions;
+
+namespace Ordering.Domain.ValueObjects
 {
     public record OrderId
     {
         public Guid Value { get; }
+
+        private OrderId(Guid value) => Value = value;
+
+        public static OrderId Of(Guid value)
+        {
+            ArgumentNullException.ThrowIfNull(value);
+
+            if (value == Guid.Empty)
+            {
+                throw new DomainException("CustomerId cannot be empty.");
+            }
+
+            return new OrderId(value);
+        }
     }
 }
--- src/Services/Ordering/Ordering.Domain/ValueObjects/OrderItemId.cs
+++ src/Services/Ordering/Ordering.Domain/ValueObjects/OrderItemId.cs
@@ -1,7 +1,23 @@
-﻿namespace Ordering.Domain.ValueObjects
+﻿using Ordering.Domain.Exceptions;
+
+namespace Ordering.Domain.ValueObjects
 {
     public record OrderItemId
     {
         public Guid Value { get; }
+
+        private OrderItemId(Guid value) => Value = value;
+
+        public static OrderItemId Of(Guid value)
+        {
+            ArgumentNullException.ThrowIfNull(value);
+
+            if (value == Guid.Empty)
+            {
+                throw new DomainException("CustomerId cannot be empty.");
+            }
+
+            return new OrderItemId(value);
+        }
     }
 }
