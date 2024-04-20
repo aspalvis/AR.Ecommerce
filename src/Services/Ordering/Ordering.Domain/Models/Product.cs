cdab9fdfbd1015959f7afb65a13055ec3c4664f6
Andris Spalvis <spalvis.andris@gmail.com>
2024-04-20 19:02:24 +0300

    Ordering.Domain - Order methods

--- src/Services/Ordering/Ordering.Domain/Models/Product.cs
+++ src/Services/Ordering/Ordering.Domain/Models/Product.cs
@@ -7,5 +7,19 @@ namespace Ordering.Domain.Models
     {
         public string Name { get; private set; } = default!;
         public decimal Price { get; private set; } = default!;
+
+        public static Product Create(ProductId id, string name, decimal price)
+        {
+            ArgumentNullException.ThrowIfNull(id, nameof(id));
+            ArgumentException.ThrowIfNullOrWhiteSpace(name, nameof(name));
+            ArgumentOutOfRangeException.ThrowIfNegativeOrZero(price);
+
+            return new Product
+            {
+                Name = name,
+                Price = price,
+                Id = id
+            };
+        }
     }
 }
